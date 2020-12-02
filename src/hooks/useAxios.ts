48122a511513f239496363f328460c72193fe98b
zettle <719925519@qq.com>
2020-12-02 08:42:25 +0800

    'ajax'

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
-import {ref, reactive, toRefs} from 'vue';
+import {ref, reactive, Ref, toRefs} from 'vue';
 
 axios.defaults.timeout = 120000; // 超时时间，单位ms，这里设置为2分钟
 const http = axios.create();
@@ -15,6 +15,15 @@ http.interceptors.response.use(<T>(response: AxiosResponse) => {
 export default function () {
     return {
         get<T> (url: string) {
+            const resp: Ref<T | null> = ref(null);
+            const isLoading = ref(true);
+            http.get<T>(url).then(res => {
+                resp.value = res.data;
+                isLoading.value = false;
+            });
+            return { resp, isLoading };
+        },
+        getSync<T> (url: string) {
             return http.get<T>(url);
         }
     };
--- src/pages/demo/ajax.vue
+++ src/pages/demo/ajax.vue
@@ -1,13 +1,16 @@
 <template>
     <m-layout>
         <m-button size="sm" @click="sendOne">发起1个请求</m-button>
+        <ul v-if="list">
+            <li v-for="item of list" :key="item.code">{{item.name}}</li>
+        </ul>
     </m-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, Ref } from 'vue';
 import useAxios from '@hooks/useAxios';
-
+// import axios from 'axios';
 // 字典项接口
 interface FetchDicListType {
     code: string;
@@ -15,14 +18,21 @@ interface FetchDicListType {
 }
 export default defineComponent({
     setup () {
+        // const list: Ref<FetchDicListType[]> = ref(null);
+        // axios.get('/api/common/getDicList').then(res => {
+        //     const data = res.data.data as FetchDicListType[];
+        //     list.value = data;
+        // });
+        // return { list };
         const axios = useAxios();
+        const { isLoading, resp: list } = axios.get<FetchDicListType[]>('/api/common/getDicList');
+        console.log(isLoading);
 
-        const sendOne = () => {
-            const resp = axios.get<FetchDicListType[]>('/api/common/getDicList');
-            console.log('resp', resp);
+        const sendOne = async () => {
+            const resp = await axios.getSync<FetchDicListType[]>('/api/common/getDicList');
+            console.log(resp);
         };
-
-        return { sendOne };
+        return { list, sendOne };
     }
 });
 </script>
